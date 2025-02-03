340a95ae2428c8e51960454b839999a640193e40
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2025-02-03 07:43:26 +0000

    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pygwalker.api.streamlit import StreamlitRenderer
 st.set_page_config(layout="wide")
 st.write("It's alive!")
 
-uploaded_file = st.file_uploader("Add your ";"-delimited, utf-8-encoded csv data")
+uploaded_file = st.file_uploader("Add your ;-delimited, utf-8-encoded csv data")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, delimiter=";")
