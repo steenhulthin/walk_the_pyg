8c266b7960d29fcf17c16350a9a15c0b2bbb1f71
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2025-02-03 07:38:54 +0000

    bugfix?

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write("It's alive!")
 
 uploaded_file = st.file_uploader("Add your csv data")
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file, delimiter=';')
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, delimiter=";")
     pyg_app = StreamlitRenderer(df)
     pyg_app.explorer()
